984aefd6c49227aa45c31aa4ca8a05a9391b5ea8
wutong03@iwaimai.baidu.com <wutong03@iwaimai.baidu.com>
2017-02-22 11:00:10 +0800

    add note

--- Vue.js
+++ Vue.js
@@ -1,6 +1,7 @@
 function Vue(option) {
     this.init(option);
 }
+
 Vue.prototype.init = function(option) {
     this._data = option.data || {};
     this._method = option.method;
@@ -11,6 +12,7 @@ Vue.prototype.init = function(option) {
     this.react();
     this.initMvvM();
 }
+// 将dom中的变量、dom和data中的变量对应
 Vue.prototype.bind = function() {
     for (var i = 0, length = this.elements.length; i < length; i++) {
         var ele = this.elements[i],
@@ -20,6 +22,10 @@ Vue.prototype.bind = function() {
             var span = '<span v-data="' + b + '"></span>';
             return span;
         })
+        /* <div id="template">
+        *  <span v-data="hello"></span>
+        *  </div> 
+        */
         ele.innerHTML = html;
         spans = ele.querySelectorAll('[v-data]');
         for (var j = 0, l = spans.length; j < l; j++) {
@@ -35,6 +41,7 @@ Vue.prototype.bind = function() {
         }
     }
 }
+// 观察_data对象 data的变化可以引起dom中对应的变量变化
 Vue.prototype.observe = function() {
     var self = this,
         eles;
@@ -56,6 +63,7 @@ Vue.prototype.observe = function() {
         })
     }
 }
+// 将input中的value赋值给_data中的对应变量
 Vue.prototype.initMvvM = function() {
     var self = this;
     var models = document.querySelectorAll('[v-model]'),
@@ -66,6 +74,7 @@ Vue.prototype.initMvvM = function() {
         self._data[dataModel] = model.value;
     }
 }
+// 响应事件，改变_data中的对应变量的值
 Vue.prototype.react = function() {
     var self = this;
     var models = document.querySelectorAll('[v-model]'),
